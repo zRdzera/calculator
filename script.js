011b4625ebc881476cdf22a2f609752f5dce82d2
Everton Zardo <evezardo15@gmail.com>
2022-06-14 13:24:48 -0300

    Basic operations working Added an event listener to the calculator buttons (except Clear and Delete) Done the basic calculator operations, making them work correctly and displaying the result on the screen.

--- script.js
+++ script.js
@@ -17,25 +17,72 @@ function divide(firstNumber, secondNumber){
 function operate(a, b, operator){
     switch (operator) {
         case '+':
-            add(a, b);
+            return add(a, b);
             break;
 
         case '-':
-            subtract(a, b);
+            return subtract(a, b);
             break;
 
         case '*':
-            multiply(a, b);
+            return multiply(a, b);
             break;
 
         case '/':
-            divide(a, b);
-            break;
-    
-        default:
-            alert("INVALID OPERATOR!")
+            return divide(a, b);
             break;
     }
 }
 
 const display = document.querySelector('#display');
+const displayOperation = document.querySelector('#operation');
+const displayResult = document.querySelector('#result');
+const buttons = document.querySelectorAll('.button');
+let firstNumber = 0;
+let operator;
+let secondNumber = 0;
+
+buttons.forEach(button => 
+{
+    if(button.textContent !== 'Clear' && button.textContent !== 'Delete')
+        button.addEventListener('click', displayNumbers);
+})
+
+function displayNumbers(e){
+    let buttonClicked = e.target.textContent;
+    let numbersRegex = new RegExp ("^([0-9])$");
+
+    if(operator === undefined){
+        if(numbersRegex.test(buttonClicked)){   
+            firstNumber += buttonClicked;
+            display.textContent += buttonClicked;
+        }
+        else {
+            if(buttonClicked !== '=' && firstNumber !== 0){
+                operator = buttonClicked;
+                display.textContent += `   ${buttonClicked}   `;   
+            }
+        }
+    }
+    else {
+        if(numbersRegex.test(buttonClicked)){   
+            secondNumber += buttonClicked;
+            display.textContent += buttonClicked; 
+        }
+    }
+
+    if(buttonClicked === '='){
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+
+        console.log(firstNumber);
+        console.log(secondNumber);
+
+        display.textContent = operate(firstNumber, secondNumber, operator);
+
+        // RESET ALL VARIABLES;
+        firstNumber = 0;
+        secondNumber = 0;
+        operator = undefined;
+    }
+}
